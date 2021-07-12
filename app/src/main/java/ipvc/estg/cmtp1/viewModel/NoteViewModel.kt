5026adea4b320394a4f7e71af1484688fd3ce884
joaoboas98 <62546037+joaoboas98@users.noreply.github.com>
2021-07-12 04:53:36 +0100

    Listar

--- app/src/main/java/ipvc/estg/cmtp1/viewModel/NoteViewModel.kt
+++ app/src/main/java/ipvc/estg/cmtp1/viewModel/NoteViewModel.kt
@@ -14,13 +14,11 @@ class NoteViewModel(application: Application) : AndroidViewModel(application)  {
 
     private val repository: NoteRepository
     val allNotes: LiveData<List<Note>>
-
     init {
         val notesDao = NoteDB.getDatabase(application, viewModelScope).noteDao()
         repository = NoteRepository(notesDao)
         allNotes = repository.allNotes
     }
-
     fun insert(note: Note) = viewModelScope.launch(Dispatchers.IO) {
         repository.insert(note)
     }
