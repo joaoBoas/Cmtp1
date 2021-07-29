90dd15e49d2529153340f1d617c72b20a5e15cef
joaoboas98 <62546037+joaoboas98@users.noreply.github.com>
2021-07-29 03:28:22 +0100

    Pontos no mapa update

--- app/src/main/java/ipvc/estg/cmtp1/api/EndPoints.kt
+++ app/src/main/java/ipvc/estg/cmtp1/api/EndPoints.kt
@@ -11,4 +11,15 @@ interface EndPoints {
     fun login(@Field( "username") username: String?, @Field("password") password: String?): Call<User>
     @GET("report")
     fun getReports(): Call<List<Report>>
+    @FormUrlEncoded
+    @POST("report/addReport")
+    fun addReport(@Field("title") title: String,
+                  @Field("description") description: String,
+                  @Field("lat") lat: Float,
+                  @Field("lng") lng: Float,
+                  @Field("date_creation") date_creation: String,
+                  @Field("type") type: String,
+                  @Field("user_id") user_id: Int
+    ): Call<Report>
+
 }
